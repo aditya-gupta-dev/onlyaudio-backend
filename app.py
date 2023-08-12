617de661bbb96b3169c6a7ca7cf70ae10f8c5c41
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-12 20:35:15 +0530

    Create app.py
    added the source code for the api

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, Response, jsonify
+from requests import get
+import pytube
+import os
+from flask_cors import CORS
+
+key = os.getenv('key')
+url = 'https://www.googleapis.com/youtube/v3/videos'
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+  return {"message": "index page"}
+
+
+@app.route("/video")
+def vid():
+  query = request.headers.get('q')
+  maxResults = request.headers.get('maxResults')
+
+  if maxResults == None:
+    maxResults = 5
+
+  if query == None:
+
+    message = {"message": "query not provided"}
+
+    return Response(status=400, response=jsonify(message))
+  else:
+    base_url = 'https://www.googleapis.com/youtube/v3/search'
+
+    params = {
+      'part': 'snippet',
+      'q': query,
+      'type': 'video',
+      'maxResults': maxResults,
+      'key': key
+    }
+    vid_info = []
+    items = get(base_url, params=params).json()["items"]
+
+    for item in items:
+      vid_info.append({
+        'id':
+        item['id']['videoId'],
+        'thumbnailUrl':
+        item['snippet']['thumbnails']['default']['url'],
+        'title':
+        item['snippet']['title']
+      })
+
+    return vid_info
+
+
+@app.route('/audio')
+def audio():
+  id = request.headers.get('id')
+  if id is None:
+    return {"message": "id of the youtube video not provided"}
+  else:
+    yt = pytube.YouTube(f'https://youtu.be/{id}')
+    url = yt.streams.get_audio_only().url
+    return {"url": url}
+
+
+app.run(host='0.0.0.0', port=81)
